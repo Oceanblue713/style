ae0372345094295f164a44b3084d9581f03486c8
Anri Liy <amedamachip713@gmail.com>
2024-01-30 21:36:47 -0800

    Container

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { Sidebar } from "./components/Sidebar";
+import { Rightbar } from "./components/Rightbar";
+import { Feed } from "./components/Feed";
+import { Box, Container } from "@mui/material";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Container sx={{ backgroundColor: "red" }}>
+      <Sidebar />
+      <Feed />
+      <Rightbar />
+    </Container>
+  );
 };
 
 export default App;
